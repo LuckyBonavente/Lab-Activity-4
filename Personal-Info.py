5a56f7dd2070d46e0b217ceaff192e2997767631
LuckyBonavente <82501067+LuckyBonavente@users.noreply.github.com>
2021-04-14 16:09:31 +0700

    Add files via upload

---
+++ Personal-Info.py
@@ -0,0 +1,6 @@
+firstName = input("What is your First Name? ")
+lastName = input("What is your Last Name? ")
+location = input("What is your Location? ")
+age = input("What is your Age? ")
+space = " "
+print("Hello. I am"+space+firstName+space+lastName+"! I live in"+space+location+". I am"+space+age+space+"years old")
